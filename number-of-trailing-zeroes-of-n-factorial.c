55962cf9835136518753d3e777bd1f8d91b1ad29
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-11-05 01:46:25 +0000

    Improve solution
    Now it is fast enough!

--- number-of-trailing-zeroes-of-n-factorial.c
+++ number-of-trailing-zeroes-of-n-factorial.c
@@ -3,21 +3,24 @@
 long
 zeros(long n)
 {
-  static char pattern2[] = { 1, 3, 4, 7, 8, 10, 11, 15, 16, 18, 19, 22, 23, 25, 26 };
-  static char pattern5[] = { 1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28 };
+  static char pattern2[] = { 0, 1, 3, 4, 7, 8, 10, 11, 15, 16, 18, 19, 22, 23, 25, 26 };
+  static char pattern5[] = { 0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28 };
 
-  long twos = pattern2[(n / 2) % 16 - 1]
-    + (n / 32) * LAST(pattern2);
-  long fives = pattern5[(n / 5) % 25 - 1]
-    + (n / 125) * LAST(pattern5);
+  long twos = 0, fives = 0;
 
-  for (long i = 32; i <= n; i += 32)
-    for (long tmp = i; tmp % 2 == 0; tmp /= 2)
-      ++twos;
+  for (long i = 2, j = 0; i <= n; i *= 16, j += 4)
+    {
+      long ind = n / i % 16;
+      twos += j * ind + pattern2[ind]
+        + n / (i * 16) * (LAST(pattern2) + j * 15);
+    }
 
-  for (long i = 125; i <= n; i += 125)
-    for (long tmp = i; tmp % 5 == 0; tmp /= 5)
-      ++fives;
+  for (long i = 5, j = 0; i <= n; i *= 25, j += 2)
+    {
+      long ind = n / i % 25;
+      fives += j * ind + pattern5[ind]
+        + n / (i * 25) * (LAST(pattern5) + j * 24);
+    }
 
   return twos < fives ? twos : fives;
 }
