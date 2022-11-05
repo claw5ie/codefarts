d45203b711c4743a187de735cc85322eb044e81e
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-11-05 14:00:58 +0000

    Improve solution even more

--- number-of-trailing-zeroes-of-n-factorial.c
+++ number-of-trailing-zeroes-of-n-factorial.c
@@ -3,17 +3,9 @@
 long
 zeros(long n)
 {
-  static char pattern2[] = { 0, 1, 3, 4, 7, 8, 10, 11, 15, 16, 18, 19, 22, 23, 25, 26 };
   static char pattern5[] = { 0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28 };
 
-  long twos = 0, fives = 0;
-
-  for (long i = 2, j = 0; i <= n; i *= 16, j += 4)
-    {
-      long ind = n / i % 16;
-      twos += j * ind + pattern2[ind]
-        + n / (i * 16) * (LAST(pattern2) + j * 15);
-    }
+  long fives = 0;
 
   for (long i = 5, j = 0; i <= n; i *= 25, j += 2)
     {
@@ -22,5 +14,5 @@ zeros(long n)
         + n / (i * 25) * (LAST(pattern5) + j * 24);
     }
 
-  return twos < fives ? twos : fives;
+  return fives;
 }
