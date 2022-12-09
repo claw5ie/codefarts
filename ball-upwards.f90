7ca223728077b8465de2ee7b12da0f85626a56ed
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-12-09 20:34:09 +0000

    Add ball-upwards.f90

---
+++ ball-upwards.f90
@@ -0,0 +1,27 @@
+module solution
+  implicit none
+
+contains
+  function maxball(v0) result(res)
+    real :: v0, tmax
+    real :: lower, upper
+    integer :: res
+
+    tmax = v0 * 1000 / (3600 * 2.0 * 0.5 * 9.81)
+    lower = int(tmax * 10) / 10.0
+    upper = (int(tmax * 10) + 1) / 10.0
+
+    if (abs(tmax - lower) > abs(tmax - upper)) then
+       lower = upper
+    end if
+
+    res = nint(lower * 10)
+  end function maxball
+end module solution
+
+program hello
+  use solution
+  integer :: n
+  n = maxball(15.0)
+  print *, n
+end program hello
